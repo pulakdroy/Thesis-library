edc6e2f55a6716d3fb6b56155f0719168e0af772
pulakdroy <pulakdr@gmail.com>
2023-12-05 16:11:43 +0600

    4th

---
+++ edit_profile.php
@@ -0,0 +1,113 @@
+<?php
+
+require_once("DBconnect.php");
+
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php"); // Redirect to login page if not logged in
+    exit();
+}
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Sanitize and validate the input data as needed
+
+    // Update user information in the database
+    $userId = $_SESSION['user_id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone_number = $_POST['phone_number'];
+    $department = $_POST['department'];
+
+    // Update the user details in the database (modify this query based on your database structure)
+    $updateSql = "UPDATE create_account SET name = '$name', email = '$email', phone_number = '$phone_number', department = '$department' WHERE id = '$userId'";
+
+    if (mysqli_query($conn, $updateSql)) {
+        // Update successful, you might want to redirect to the profile page or show a success message
+        header("Location: profile.php");
+        exit();
+    } else {
+        // Handle the case where the update fails
+        $error_message = "Error updating user information: " . mysqli_error($conn);
+    }
+}
+
+// Fetch user details for pre-filling the form
+$studentId = $_SESSION['user_id'];
+$fetchUserSql = "SELECT * FROM create_account WHERE student_id = '$studentId'";
+$result = mysqli_query($conn, $fetchUserSql);
+
+
+
+if ($result && mysqli_num_rows($result) > 0) {
+    $userDetails = mysqli_fetch_assoc($result);
+} else {
+    // Handle the case where user details couldn't be fetched
+    $error_message = "Error fetching user information";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Profile</title>
+    <link rel="stylesheet" href="profile.css"> <!-- Add your stylesheet link here -->
+</head>
+<body>
+
+<header>
+    <nav class="navbar">
+        <div class="logo">
+            <h2>Thesis Library</h2>
+        </div>
+        <!-- Add any navigation links if needed -->
+        <div class="navmenu">
+            <ul>
+                <li><a href="Homepage.php">Home</a></li>
+                <li><a href="supervisor.php">Supervisors</a></li>
+                <li><a href="resources1.php">Resources</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div class="profile">
+    <h1>Edit Profile</h1>
+
+    <?php
+    if (isset($error_message)) {
+        echo '<p style="color: red;">' . $error_message . '</p>';
+    }
+    ?>
+
+    <form method="post" action="">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" value="<?php echo isset($userDetails['name']) ? $userDetails['name'] : ''; ?>" required>
+
+        <label for="student_id">Student ID:</label>
+        <input type="text" id="student_id" name="student_id" value="<?php echo isset($userDetails['student_id']) ? $userDetails['student_id'] : ''; ?>" readonly>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" value="<?php echo isset($userDetails['email']) ? $userDetails['email'] : ''; ?>" required>
+
+        <label for="phone_number">Phone Number:</label>
+        <input type="text" id="phone_number" name="phone_number" value="<?php echo isset($userDetails['phone_number']) ? $userDetails['phone_number'] : ''; ?>" required>
+
+        <label for="department">Department:</label>
+        <input type="text" id="department" name="department" value="<?php echo isset($userDetails['department']) ? $userDetails['department'] : ''; ?>" required>
+
+        <input type="submit" value="Save Changes">
+    </form>
+
+    <p><a href="profile.php">Back to Profile</a></p>
+    <p><a href="logout.php">Logout</a></p>
+
+</div>
+
+</body>
+</html>
---
+++ homepage.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Thesis Library</title>
+    <link rel="stylesheet" href="./style1.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+</head>
+<body>
+    <div id="popup" class="popup">
+        <div class="popup-content">
+            <span class="close-btn" onclick="closePopup()">&times;</span>
+            <h2></h2>
+            <p><h5>Continue with g-suite</h5></p>
+            <form id="emailForm">
+                <input type="email" placeholder="g-suite" id="email" name="email" required>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+    <script src="script1.js"></script>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <h2>Thesis Library</h2>
+            </div>
+
+            <div class="navmenu">
+                <ul>
+                    <li><a href="landing.php">Home</a></li>
+                    <li><a href="supervisor.php">Supervisors</a></li>
+                    <li><a href="resources1.php">Resources</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="upload.php">Upload</a></li>
+
+
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div class="page">
+        <div class="page2">
+            <p>Want to see thesis paper from students?</p>
+            <div class="container">
+                <input type="text" id="search-box" placeholder="Search by Topic or Supervisor">
+                <button onclick="searchBooks()">Search</button>
+                <div class="result">
+                    <ul id="result-container"></ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="search.js"></script>
+
+
+    <script>
+
+        document.getElementById("emailForm").addEventListener("submit", function (event) {
+            event.preventDefault(); 
+            var email = document.getElementById("email").value;
+            var xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status === 200) {
+                        console.log(xhr.responseText);
+
+                        closePopup();
+                    } else {
+
+                        console.error("Error:", xhr.status, xhr.statusText);
+
+                    }
+                }
+            };
+
+            xhr.open("POST", "student_email.php", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.send("email=" + encodeURIComponent(email));
+        });
+
+        function closePopup() {
+            document.getElementById("popup").style.display = "none";
+        }
+    </script>
+
+</body>
+</html>
